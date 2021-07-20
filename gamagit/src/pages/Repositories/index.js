8b5910ed6b2aeb7806e206d5e0ac9e2a6f73bc9a
Jéssica Suelen <jessiksu@hotmail.com>
2021-07-20 19:50:46 +0100

    Renderizando repositórios e programação declarativa

--- gamagit/src/pages/Repositories/index.js
+++ gamagit/src/pages/Repositories/index.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as S from './styled';
 
 export default function Repositories() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        let respositoriesName = localStorage.getItem('repositoriesName');
+        respositoriesName = JSON.parse(respositoriesName);
+        console.log(respositoriesName);
+        setRepositories(respositoriesName);
+        localStorage.clear();
+    }, []);
+
     return (
         <S.Container>
             <S.Title>Repositórios</S.Title>
             <S.List>
-                <S.ListItem>Repositório: Nome Repositório 01</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 02</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 03</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 04</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 05</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 06</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 07</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 08</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 09</S.ListItem>
-                <S.ListItem>Repositório: Nome Repositório 10</S.ListItem>
+                { repositories.map(respository => {
+                    return (
+                        <S.ListItem>{respository}</S.ListItem>
+                    )
+                })}
+
             </S.List>
         </S.Container>
     )
